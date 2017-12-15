7faaf3be9cd6c6a4ceae2cd5ef760a50e4adfd82
sipian <harshaga97@gmail.com>
2017-12-15 20:04:04 +0530

    add chmod

--- bootstrap/change.sh
+++ bootstrap/change.sh
@@ -4,4 +4,4 @@ chmod +x bootstrap
 chmod +x bootstrap-common-functions
 chmod +x bootstrap-deb-functions
 chmod +x bootstrap-src-functions
-./bootstrap develop --config-file=/home/ubuntu/bootstrap-config-file
+./bootstrap develop --config-file=/home/ubuntu/autoscaling-sfc/bootstrap/bootstrap-config-file
