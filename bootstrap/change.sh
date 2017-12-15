5576304b3238d1db1c755ff49fd14db085cae63b
sipian <harshaga97@gmail.com>
2017-12-15 22:24:53 +0530

    make bootstrap in develop mode

--- bootstrap/change.sh
+++ bootstrap/change.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+
+ip=$(ip -f inet -o addr show ens3|cut -d\  -f 7 | cut -d/ -f 1;)
+
+line=$(sudo grep -n "rabbitmq_broker_ip=" bootstrap-config-file | sed 's/^\([0-9]\+\):.*$/\1/')
+sudo sed -i "${line}s/.*/rabbitmq_broker_ip=${ip}/" bootstrap-config-file
+
+
 chmod +x bootstrap
 chmod +x bootstrap-common-functions
 chmod +x bootstrap-deb-functions
